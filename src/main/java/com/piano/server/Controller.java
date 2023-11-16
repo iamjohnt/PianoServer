dec12f7345610f1c0bdb544db95a58ddb5d462a0
John Tran <johntrran@Johns-MacBook-Air.local>
2023-11-16 11:05:21 -0500

    added functionality to recieve chord data

--- src/main/java/com/piano/server/Controller.java
+++ src/main/java/com/piano/server/Controller.java
@@ -1,8 +1,17 @@
 package com.piano.server;
 
+import com.piano.server.in.Chord;
+import com.piano.server.in.Hello;
+import com.piano.server.out.ChordResponse;
+import com.piano.server.out.Response;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
 import org.springframework.web.util.HtmlUtils;
+
+import java.sql.Timestamp;
+import java.time.Instant;
+import java.util.Calendar;
+import java.util.Date;
 
 @org.springframework.stereotype.Controller
 public class Controller {
@@ -13,5 +22,14 @@ public class Controller {
     public Response greeting(Hello message) throws Exception {
         return new Response("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
+
+    @MessageMapping("/chord")
+    @SendTo("/topic/chord")
+    public ChordResponse chordResponse(Chord message) throws Exception {
+        Timestamp ts = Timestamp.from(Instant.now());
+        System.out.println(ts.toString() + " LOG: received chord --- " + message.getChord().toString());
+        return new ChordResponse("dummy response");
+    }
+
 
 }
---
+++ src/main/java/com/piano/server/in/Chord.java
@@ -0,0 +1,31 @@
+package com.piano.server.in;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import java.util.List;
+
+public class Chord {
+
+    @JsonProperty("chord")
+    private List<Integer> chord;
+
+    public Chord() {
+    }
+
+    public Chord(List<Integer> chord) {
+        this.chord = chord;
+    }
+
+    public List<Integer> getChord() {
+        return chord;
+    }
+
+    public void setChord(List<Integer> chord) {
+        this.chord = chord;
+    }
+
+    @Override
+    public String toString() {
+        return chord.toString();
+    }
+}

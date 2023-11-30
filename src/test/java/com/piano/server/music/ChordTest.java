50f4f8eb4f5f5ed429f8141c051203c1c1e29060
John Tran <johntrran@Johns-MacBook-Air.local>
2023-11-30 15:17:59 -0500

    added unite test ChordTest for Chord class

---
+++ src/test/java/com/piano/server/music/ChordTest.java
@@ -0,0 +1,50 @@
+package com.piano.server.music;
+
+import com.piano.server.game.music.Chord;
+import org.junit.jupiter.api.Test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ChordTest {
+
+    @Test
+    public void test_equals() {
+        Chord one = new Chord(10, 50, 32);
+        Chord two = new Chord(50, 32, 10);
+        assert (one.equals(two));
+    }
+
+    @Test
+    public void test_note_equals() {
+        Chord one = new Chord(10, 50, 32);
+        Chord two = new Chord(33, 12, 54);
+        assert (!one.equals(two));
+    }
+
+    @Test
+    public void test_to_string() {
+        Chord chord = new Chord(21, 22);
+        assertEquals(chord.toString(), "21 22 ");
+    }
+
+
+    @Test
+    public void test_set_using_set() {
+        Set<Integer> set = new HashSet<>();
+        set.add(21);
+        set.add(22);
+        Chord actual = new Chord(set);
+        Chord expected = new Chord(21,22);
+        assertEquals(actual, expected);
+    }
+
+    @Test
+    public void test_set_using_array_with_dupes() {
+        Chord actual = new Chord(21, 22, 22);
+        Chord expected = new Chord(21, 22);
+        assertEquals(expected, actual);
+    }
+}

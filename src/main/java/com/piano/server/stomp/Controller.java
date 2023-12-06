aae11cc8b15b411ab3720fd2ea9d7ddba0efe646
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-06 15:25:25 -0500

    added autowired GameSessionContainer and ability to get session id from client as parameter in Controller

--- src/main/java/com/piano/server/stomp/Controller.java
+++ src/main/java/com/piano/server/stomp/Controller.java
@@ -1,19 +1,28 @@
 package com.piano.server.stomp;
 
+import com.piano.server.game.session.GameSessionContainer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.Header;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
+import org.springframework.stereotype.Component;
 import org.springframework.web.util.HtmlUtils;
 
 import java.sql.Timestamp;
 import java.time.Instant;
 
 @org.springframework.stereotype.Controller
+
+@Component
 public class Controller {
 
+    @Autowired
+    private GameSessionContainer gameSessions;
 
     @MessageMapping("/hello")
     @SendTo("/topic/greetings")
-    public TestResponse greeting(TestSubmission message) throws Exception {
+    public TestResponse greeting(@Header("simpSessionId") String sessionId, TestSubmission message) throws Exception {
+        System.out.println(sessionId);
         return new TestResponse("Hello, " + HtmlUtils.htmlEscape(message.getName()) + "!");
     }
 
@@ -27,7 +36,10 @@ public class Controller {
 
     @MessageMapping("/settings")
     @SendTo("/topic/chord")
-    public GameSettingsResponse chordResponse(GameSettingsSubmission message) throws Exception {
+    public GameSettingsResponse chordResponse(@Header("simpSessionId") String sessionId, GameSettingsSubmission message) throws Exception {
+//        Config config = new Config();
+//        GameSession gameSession = new GameSession(config);
+//        this.gameSessions.addSession(sessionId, gameSession);
         Timestamp ts = Timestamp.from(Instant.now());
         System.out.println("\n" + ts.toString() + " LOG: received settings --- \n" + message.toString() + "\n");
         return new GameSettingsResponse(true);

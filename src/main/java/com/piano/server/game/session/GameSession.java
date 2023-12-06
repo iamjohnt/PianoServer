29b6c27397afed9187e1ff7a90535405118d24d8
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-06 15:23:08 -0500

    create GameSession and basic unit test printing its toString()

---
+++ src/main/java/com/piano/server/game/session/GameSession.java
@@ -0,0 +1,39 @@
+package com.piano.server.game.session;
+
+import com.piano.server.game.music.Chord;
+import com.piano.server.game.music.Config;
+import com.piano.server.game.music.MusicMakable;
+import com.piano.server.game.music.MusicMakerFactory;
+
+import java.util.Deque;
+
+public class GameSession {
+
+    private MusicMakable musicMaker;
+    private String session_id;
+    private int session_user_id;
+    private Deque<Chord> music;
+    private int curChordPointer;
+
+    public GameSession() {
+        session_id = "1";
+    }
+
+    public GameSession(String session_id, Config config) {
+        MusicMakerFactory factory = new MusicMakerFactory();
+        this.musicMaker = factory.buildMusicMaker(config);
+        this.session_id = session_id;
+    }
+
+    public String getSession_id() {
+        return session_id;
+    }
+
+    public void startGame() {
+        music = musicMaker.makeMusic();
+    }
+
+    public String toString() {
+        return "Session ID: " + session_id + " : Music: " + music.toString();
+    }
+}
---
+++ src/test/java/com/piano/server/session/GameSessionTest.java
@@ -0,0 +1,32 @@
+package com.piano.server.session;
+
+import com.piano.server.game.music.Config;
+import com.piano.server.game.session.GameSession;
+import com.piano.server.game.util.ChordPool;
+import com.piano.server.game.util.KeySigMode;
+import com.piano.server.game.util.KeySigNote;
+import com.piano.server.game.util.WhichHands;
+import org.junit.jupiter.api.Test;
+
+public class GameSessionTest {
+
+    @Test
+    void test_print_session() {
+        Config config = new Config()
+                .setChordPool(ChordPool.NOTE_INTERVAL_TRIAD_TETRAD)
+                .setKeySigNote(KeySigNote.C)
+                .setKeySigMode(KeySigMode.MAJOR)
+                .setHands(WhichHands.BOTH)
+                .setLeftMin(24)
+                .setLeftMax(48)
+                .setRightMin(60)
+                .setRightMax(84)
+                .setLength(10);
+
+        String session_id = "qwerasdf1234";
+        GameSession session = new GameSession(session_id, config);
+        session.startGame();
+        System.out.println(session);
+    }
+
+}

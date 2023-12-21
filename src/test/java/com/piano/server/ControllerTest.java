c164a116a12c680b66af2783657102accc13a75c
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-21 15:27:39 -0500

    unit tested Controller.handleCreateSession()

--- src/test/java/com/piano/server/ControllerTest.java
+++ src/test/java/com/piano/server/ControllerTest.java
@@ -1,5 +1,11 @@
 package com.piano.server;
 
+import com.piano.server.game.session.GameSessionContainer;
+import com.piano.server.game.util.*;
+import com.piano.server.stomp.Controller;
+import com.piano.server.stomp.response.CreateSessionResponse;
+import com.piano.server.stomp.submission.CreateSessionSubmission;
+import com.piano.server.stomp.submission.GameSettingsSubmission;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -8,15 +14,36 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class ControllerTest {
 
+    private Controller controller;
+    private String sessionIdTest;
+    private GameSettingsSubmission defaultSettings;
+
     @BeforeEach
     void setUp() {
+        this.controller = new Controller();
+        this.controller.setGameSessions(new GameSessionContainer());
+        this.controller.setGameState(new GameState(GameState.State.UNSTARTED));
+        this.sessionIdTest = "qwerasdf1234";
+        this.defaultSettings = new GameSettingsSubmission()
+            .setChordPool(ChordPool.NOTE)
+            .setKeySigNote(KeySigNote.C)
+            .setKeySigMode(KeySigMode.MAJOR)
+            .setWhichHands(WhichHands.RIGHT)
+            .setRightMin(60)
+            .setRightMax(60)
+            .setLength(3);
     }
 
     @AfterEach
     void tearDown() {
+        this.controller = null;
+        this.defaultSettings = null;
     }
 
     @Test
-    void greeting() {
+    void test_create_session() {
+        CreateSessionSubmission submission = new CreateSessionSubmission();
+        CreateSessionResponse response = controller.handleCreateSession(sessionIdTest, submission);
+        assertTrue(response.getIsCreateSessionSuccess());
     }
 }

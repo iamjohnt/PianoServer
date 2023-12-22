807586964b199d842daade4c9d12780230e9a0d7
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-22 12:09:16 -0500

    unit tested adding game settings to game session

--- src/test/java/com/piano/server/ControllerTest.java
+++ src/test/java/com/piano/server/ControllerTest.java
@@ -3,10 +3,7 @@ package com.piano.server;
 import com.piano.server.game.session.GameSessionContainer;
 import com.piano.server.game.util.*;
 import com.piano.server.stomp.Controller;
-import com.piano.server.stomp.response.CreateSessionResponse;
-import com.piano.server.stomp.response.EndSessionResponse;
-import com.piano.server.stomp.response.EndSessionSubmission;
-import com.piano.server.stomp.response.StartGameResponse;
+import com.piano.server.stomp.response.*;
 import com.piano.server.stomp.submission.CreateSessionSubmission;
 import com.piano.server.stomp.submission.GameSettingsSubmission;
 import com.piano.server.stomp.submission.StartGameSubmission;
@@ -60,5 +57,16 @@ public class ControllerTest {
         System.out.println(response.getMessage());
         assertTrue(response.isEndSessionSuccess());
     }
+
+    @Test
+    void test_game_session_recieve_settings() {
+        CreateSessionSubmission submission = new CreateSessionSubmission();
+        controller.handleCreateSession(sessionIdTest, submission);
+
+        GameSettingsResponse response = controller.handleGameSettings(sessionIdTest, defaultSettings);
+        assertTrue(response.isSuccess());
+    }
+
+
 
 }

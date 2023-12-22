2dd50c228fdb02099c75b6e8de637548c9f360a0
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-22 12:09:57 -0500

    added boolean isEndSessionSuccess to EndSessionResponse

--- src/main/java/com/piano/server/stomp/response/EndSessionResponse.java
+++ src/main/java/com/piano/server/stomp/response/EndSessionResponse.java
@@ -2,8 +2,14 @@ package com.piano.server.stomp.response;
 
 public class EndSessionResponse {
 
+    private boolean isEndSessionSuccess;
     private String sessionId;
     private String message;
+
+    public EndSessionResponse(boolean isEndSessionSuccess, String message) {
+        this.isEndSessionSuccess = isEndSessionSuccess;
+        this.message = message;
+    }
 
     public String getMessage() {
         return message;
@@ -19,5 +25,13 @@ public class EndSessionResponse {
 
     public void setSessionId(String sessionId) {
         this.sessionId = sessionId;
+    }
+
+    public boolean isEndSessionSuccess() {
+        return isEndSessionSuccess;
+    }
+
+    public void setEndSessionSuccess(boolean endSessionSuccess) {
+        isEndSessionSuccess = endSessionSuccess;
     }
 }

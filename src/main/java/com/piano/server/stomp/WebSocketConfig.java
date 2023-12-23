c3871d0254f44a7c2a2b2bd4806c55f43ba1498a
John Tran <johntrran@Johns-MacBook-Air.local>
2023-12-23 12:44:28 -0500

    changed WebSocketConfig to allow new /queue broker dest, and allow /user prefixes

--- src/main/java/com/piano/server/stomp/WebSocketConfig.java
+++ src/main/java/com/piano/server/stomp/WebSocketConfig.java
@@ -18,8 +18,9 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
 
     @Override
     public void configureMessageBroker(MessageBrokerRegistry config) {
-        config.enableSimpleBroker("/topic");
+        config.enableSimpleBroker("/topic", "/queue");
         config.setApplicationDestinationPrefixes("/app");
+        config.setUserDestinationPrefix("/user");
     }
 
     @Override
